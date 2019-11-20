75b31118921625f9222d85648572ae71b1a86a28
Minzhuo Jin <minzhuo.jin@gmail.com>
2019-11-19 23:00:13 -0700

    Update installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 apt-get update
-apt-get install -y python-pip mininet ethtool netcat moreutils
+apt-get install -y python-pip mininet ethtool netcat moreutils tcpdump
 pip install -r requirements.txt
+
+# fix analyze.py dependencies
+apt install python-backports.functools-lru-cache
 
 # this fixes mininet bug with ovs-controller
 apt-get install -y openvswitch-testcontroller
