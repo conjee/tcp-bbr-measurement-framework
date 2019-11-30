f69efa71e820d3dd003776ed18816602123aa07f
Minzhuo Jin <mzjin@debian>
2019-11-30 11:01:56 -0700

    Fix uncaptured  initial cwnd values

--- ss_script.sh
+++ ss_script.sh
@@ -1,5 +1,5 @@
 while true;
 do
-    ss -tinH dst 10.2/16 | sed -n -e 's/.* cwnd:\([0-9]*\).* bbr:(\([^)]*\)).*/\1;;\2/p' -e 's/.* cwnd:\([0-9]*\).* ssthresh:\([0-9]*\).*/\1;\2;/p';
+    ss -tinH dst 10.2/16 | sed -n -e 's/bbr.* cwnd:\([0-9]*\).* bbr:(\([^)]*\)).*/\1;;\2/p' -e 's/cubic.* cwnd:\([0-9]*\).* ssthresh:\([0-9]*\).*/\1;\2;/p' -e 's/cubic.* cwnd:\([0-9]*\).*/\1;0;/p';
     sleep $1;
 done | ts -s '%H:%M:%.S;'
